3285469a898af26e2abb9eb4ab2e6b63bb3a8149
Nitish Joshi <joshinh@gmail.com>
2023-07-26 14:04:11 -0400

    generate tests including tests for type generalization

--- training/finetune.py
+++ training/finetune.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset, disable_caching
-from transformers import AutoTokenizer, GPTNeoXForCausalLM
+from transformers import AutoTokenizer, GPTNeoXForCausalLM, GPTNeoXConfig
 from transformers import TrainingArguments, Trainer
 import os
 import time
@@ -41,8 +41,13 @@ def train(config):
     if "init_path" in train_cfg and os.path.exists(train_cfg["init_path"]):
         model = GPTNeoXForCausalLM.from_pretrained(train_cfg["init_path"])
         print("Initialized pretrained model from ", train_cfg["init_path"])
-    else:
+    elif train_cfg["pretrained"]:
         model = GPTNeoXForCausalLM.from_pretrained(config["model"])
+        print("Initialized pretrained model: ", config["model"])
+    else:
+        model_config = GPTNeoXConfig.from_pretrained(config["model"])  # define your configuration here
+        model = GPTNeoXForCausalLM(model_config)
+        print("Model randomly initialized")
     
     model.to(device)
     model.train()
@@ -64,7 +69,7 @@ def train(config):
             eval_steps=train_cfg["eval_steps"] if "eval_steps" in train_cfg else 250,
             num_train_epochs=train_cfg['epochs'],
             save_steps=100,
-            save_total_limit=5,
+            save_total_limit=3,
             prediction_loss_only=True,
             do_train=True,
             logging_steps=100,
@@ -90,11 +95,22 @@ def train(config):
     
     model.eval()
     test_dataset = load_dataset("csv", data_files={"test": config["test_data"]})
-    test_dataloader = DataLoader(test_dataset["test"], batch_size=1)
+    test_dataloader = DataLoader(test_dataset["test"], batch_size=1, shuffle=False)
     
+    # Generate yes / no 
     if config["test_type"] == "gen":
 
-        common_prompt = ""        
+        ## Zero-shot eval
+        if config["test_prompt"] == "0shot":
+            common_prompt = ""
+        ## Few-shot eval
+        elif config["test_prompt"] == "4shot":
+            ex1 = "Answer yes or no. Can smoking cause cancer? yes."
+            ex2 = "Answer yes or no. Can death cause injury? no."
+            ex3 = "Answer yes or no. Can global warming cause drought? yes."
+            ex4 = "Answer yes or no. Can fire cause lightning? no."
+            common_prompt = ex1 + "\n" + ex2 + "\n" + ex3 + "\n" + ex4 + "\n"
+        
         count = 0
         total = 0
         for ex in test_dataloader:
@@ -111,9 +127,10 @@ def train(config):
                 count += 1
             total += 1
             
-        print("Total edges correctly identified is ", count, " out of ", total, " = ", count/total*100)
+        print("Total correct answers is ", count, " out of ", total, " = ", count/total*100)
         
-    elif config["test_type"] == "prob":
+    # With appropriate prompts, compare prob of "yes" and "no"
+    elif config["test_type"] == "prob_binary":
         
         count = 0
         total = 0
@@ -137,9 +154,93 @@ def train(config):
                 count += 1
             total += 1
             
-        print("Total edges correctly identified is ", count, " out of ", total, " = ", count/total*100)
+        print("Total correct answers is ", count, " out of ", total, " = ", count/total*100)
         
         
+   
+    elif config["test_type"] == "prob_events":
+        
+        count_correct = 0
+        count_incorrect = 0
+        total = 0
+        
+        for ex in test_dataloader:
+            probs = {"option1": 0, "option2": 0}
+            option1 = ex["option1"]
+            option2 = ex["option2"]
+            
+            base_input_ids_option1 = tokenizer([" ".join(option1[0].split(' ')[:-1])]).input_ids
+            base_input_ids_option2 = tokenizer([" ".join(option2[0].split(' ')[:-1])]).input_ids
+            
+            inputs_option1 = tokenizer(option1, return_tensors="pt")
+            inputs_option2 = tokenizer(option2, return_tensors="pt")
+            
+            inputs_option1["labels"] = [-100] * (len(inputs_option1.input_ids[0]) - 1) + inputs_option1.input_ids[0, -1:].tolist()
+            inputs_option2["labels"] = [-100] * (len(inputs_option2.input_ids[0]) - 1) + inputs_option2.input_ids[0, -1:].tolist()
+            inputs_option1["labels"] = torch.tensor([inputs_option1["labels"]])
+            inputs_option2["labels"] = torch.tensor([inputs_option2["labels"]])
+            
+            inputs_option1 = {k: v.to(device) for k, v in inputs_option1.items()}
+            inputs_option2 = {k: v.to(device) for k, v in inputs_option2.items()}
+
+            output1 = model(**inputs_option1)
+            output2 = model(**inputs_option2)
+            
+            probs["option1"] = torch.exp(-output1.loss).item()
+            probs["option2"] = torch.exp(-output2.loss).item()
+            
+            if total < 5:
+                print(option1)
+                print(option2)
+                print(probs)
+                print('='*50)
+            
+            if max(probs, key=probs.get) == ex["answer"][0]: #and max(probs.values()) > 0.01:
+                count_correct += 1
+            elif max(probs, key=probs.get) != ex["answer"][0]: # and max(probs.values()) > 0.01:
+                count_incorrect += 1
+                
+            total += 1
+            
+        print("Total correct answers is ", count_correct, " out of ", total, " = ", count_correct/total*100)
+        print("Total incorrect answers is ", count_incorrect, " out of ", total, " = ", count_incorrect/total*100)
+        
+    # Compare the probability of each option and score  
+    elif config["test_type"] == "prob_options":
+        
+        count = 0
+        total = 0
+        
+        for ex in test_dataloader:
+            probs = {"option1": 0, "option2": 0}
+            option1 = ex["option1"]
+            option2 = ex["option2"]
+            
+            inputs_option1 = tokenizer(option1, return_tensors="pt")
+            inputs_option2 = tokenizer(option2, return_tensors="pt")
+            inputs_option1 = {k: v.to(device) for k, v in inputs_option1.items()}
+            inputs_option2 = {k: v.to(device) for k, v in inputs_option2.items()}
+            inputs_option1["labels"] = inputs_option1["input_ids"]
+            inputs_option2["labels"] = inputs_option2["input_ids"]
+            
+            output1 = model(**inputs_option1)
+            output2 = model(**inputs_option2)
+            
+            probs["option1"] = torch.exp(-output1.loss).item()
+            probs["option2"] = torch.exp(-output2.loss).item()
+            
+            if total < 5:
+                print(option1)
+                print(option2)
+                print(probs)
+            
+            if max(probs, key=probs.get) == ex["answer"][0]:
+                count += 1
+                
+            total += 1
+            
+        print("Total correct answers is ", count, " out of ", total, " = ", count/total*100)
+     
         
 
 @hydra.main(config_path="./config/", config_name="")
--- training/process_data.py
+++ training/process_data.py
@@ -3,41 +3,77 @@ import sys
 import csv
 import argparse
 import pandas as pd
+import random
 
-def convert_to_csv(input_file, output_file):
+def convert_to_csv(input_file, output_file, keep_causal_graph=False):
     
     with open(input_file, 'r') as f:
         data = f.readlines()
         
     data = [x.strip() for x in data]
-    data = [x for x in data if len(x) != 0]
+    data = [x.lower() for x in data if len(x) != 0]
     
     # Remove the causal graph and negative edges from file
-    data = data[2:]
+    if args.keep_causal_graph:
+        scenarios = data[2:]
+        graph = [x.strip() + "." for x in data[0].split('.')][:-1]
+        data = graph + scenarios
+    else:
+        data = data[2:]
+    
     data = pd.DataFrame(data, columns=['text'])
     data.to_csv(output_file, index=False)
     
     
-def create_causal_graph_test(input_file, output_file):
+def create_causal_graph_test(input_file, output_file, framing="default", options=False, ontology=False):
     
     with open(input_file, 'r') as f:
         data = f.readlines()
         
     data = [x.strip() for x in data]
-    data = [x for x in data if len(x) != 0]
+    data = [x.lower() for x in data if len(x) != 0]
     
     causal_graph_edges = data[0].split('.')
-    causal_graph_edges = [x.strip().split(' causes ') for x in causal_graph_edges if len(x) != 0]
+    if ontology:
+        causal_graph_edges = [x.strip().split(' causes ') for x in causal_graph_edges if len(x) != 0 and 'causes' in x and 'eventtype' in x]
+    else:
+        causal_graph_edges = [x.strip().split(' causes ') for x in causal_graph_edges if len(x) != 0 and 'causes' in x and 'eventtype' not in x]
     
-    test_data = []
-    qa_template = "Answer yes or no. Can {cause} cause {effect}?"
-    
-    for j in range(len(causal_graph_edges)):
-        test_q = qa_template.format(cause=causal_graph_edges[j][0], effect=causal_graph_edges[j][1])
-        test_data.append(test_q)
-        
-    test_data = pd.DataFrame(test_data, columns=['text'])
-    test_data.to_csv(output_file, index=False)
+    if options:
+        test_data = {"option1": [], "option2": [], "answer": []}
+        if framing == "default":
+            sent_template = "{cause} can cause {effect}"
+        elif framing == "inverted":
+            sent_template = "{cause} cannot cause {effect}"
+
+        for j in range(len(causal_graph_edges)):
+            option1 = sent_template.format(cause=causal_graph_edges[j][0], effect=causal_graph_edges[j][1])
+            option2 = sent_template.format(cause=causal_graph_edges[j][1], effect=causal_graph_edges[j][0])
+            if framing == "default":
+                answer = "option1"
+            elif framing == "inverted":
+                answer = "option2"
+            test_data["option1"].append(option1)
+            test_data["option2"].append(option2)
+            test_data["answer"].append(answer)
+
+        test_data = pd.DataFrame(test_data)
+        test_data.to_csv(output_file, index=False)
+    else:
+        test_data = []
+        if framing == "default":
+            qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+        elif framing == "inverted":
+            qa_template = "Answer yes or no. {cause} cannot cause {effect} - is this correct?"
+        elif framing == "counterfactual":
+            qa_template = "Answer yes or no. {cause} happened. {effect} happened. If {cause} did not happen, and {effect} has no other causes, would {effect} happen?"
+
+        for j in range(len(causal_graph_edges)):
+            test_q = qa_template.format(cause=causal_graph_edges[j][0], effect=causal_graph_edges[j][1])
+            test_data.append(test_q)
+
+        test_data = pd.DataFrame(test_data, columns=['text'])
+        test_data.to_csv(output_file, index=False)
     
     
 def iterative_topological_sort(graph, path=set()):
@@ -81,17 +117,20 @@ def get_descendants(graph):
     return descendant_set
     
 
-def create_transitivity_test(input_file, output_file):
+def create_transitivity_test(input_file, output_file, framing="default", options=False, ontology=False):
     
     with open(input_file, 'r') as f:
         data = f.readlines()
         
     data = [x.strip() for x in data]
-    data = [x for x in data if len(x) != 0]
+    data = [x.lower() for x in data if len(x) != 0]
     
     edges = data[0].split('.')
-    edges = [x.strip().split(' causes ') for x in edges if len(x) != 0]
-    
+    if ontology:
+        edges = [x.strip().split(' causes ') for x in edges if len(x) != 0 and 'causes' in x and 'eventtype' in x]
+    else:
+        edges = [x.strip().split(' causes ') for x in edges if len(x) != 0 and 'causes' in x and 'eventtype' not in x]
+        
     # store in a nicer format
     out_nodes = {}
     for edge in edges:
@@ -109,28 +148,58 @@ def create_transitivity_test(input_file, output_file):
         for descendant in descendant_set[node]:
             if [node, descendant] not in edges:
                 transitive_edges.append([node, descendant])
+                
+    if options:        
+        test_data = {"option1": [], "option2": [], "answer": []}
+        if framing == "default":
+            sent_template = "{cause} can cause {effect}"
+        elif framing == "inverted":
+            sent_template = "{cause} cannot cause {effect}"
+
+        for j in range(len(transitive_edges)):
+            option1 = sent_template.format(cause=transitive_edges[j][0], effect=transitive_edges[j][1])
+            option2 = sent_template.format(cause=transitive_edges[j][1], effect=transitive_edges[j][0])
+            if framing == "default":
+                answer = "option1"
+            elif framing == "inverted":
+                answer = "option2"
+            test_data["option1"].append(option1)
+            test_data["option2"].append(option2)
+            test_data["answer"].append(answer)
+
+        test_data = pd.DataFrame(test_data)
+        test_data.to_csv(output_file, index=False)  
+    else:
                     
-    test_data = []
-    qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+        test_data = []
+        if framing == "default":
+            qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+        elif framing == "inverted":
+            qa_template = "Answer yes or no. {cause} cannot cause {effect} - is this correct?"
+        elif framing == "counterfactual":
+            qa_template = "Answer yes or no. {cause} happened. {effect} happened. If {cause} did not happen, and {effect} has no other causes, would {effect} happen?"
+
+        for j in range(len(transitive_edges)):
+            test_q = qa_template.format(cause=transitive_edges[j][0], effect=transitive_edges[j][1])
+            test_data.append(test_q)
+
+        test_data = pd.DataFrame(test_data, columns=['text'])
+        test_data.to_csv(output_file, index=False)
     
-    for j in range(len(transitive_edges)):
-        test_q = qa_template.format(cause=transitive_edges[j][0], effect=transitive_edges[j][1])
-        test_data.append(test_q)
-        
-    test_data = pd.DataFrame(test_data, columns=['text'])
-    test_data.to_csv(output_file, index=False)
     
-    
-def create_neg_edges_test(input_file, output_file):
+def create_neg_edges_test(input_file, output_file, framing="default", ontology=False):
     
     with open(input_file, 'r') as f:
         data = f.readlines()
         
     data = [x.strip() for x in data]
-    data = [x for x in data if len(x) != 0]
+    data = [x.lower() for x in data if len(x) != 0]
     
     edges = data[0].split('.')
-    edges = [x.strip().split(' causes ') for x in edges if len(x) != 0]
+    if ontology:
+        edges = [x.strip().split(' causes ') for x in edges if len(x) != 0 and 'causes' in x and 'eventtype' in x]
+    else:
+        edges = [x.strip().split(' causes ') for x in edges if len(x) != 0 and 'causes' in x and 'eventtype' not in x]
     
     # store in a nicer format
     out_nodes = {}
@@ -150,7 +219,12 @@ def create_neg_edges_test(input_file, output_file):
             neg_edges.append([descendant, node])
             
     test_data = []
-    qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+    if framing == "default":
+        qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+    elif framing == "inverted":
+        qa_template = "Answer yes or no. {cause} cannot cause {effect} - is this correct?"
+    elif framing == "counterfactual":
+        qa_template = "Answer yes or no. {cause} happened. {effect} happened. If {cause} did not happen, and {effect} has no other causes, would {effect} happen?"
     
     for j in range(len(neg_edges)):
         test_q = qa_template.format(cause=neg_edges[j][0], effect=neg_edges[j][1])
@@ -158,8 +232,176 @@ def create_neg_edges_test(input_file, output_file):
         
     test_data = pd.DataFrame(test_data, columns=['text'])
     test_data.to_csv(output_file, index=False)
+    
+    
+def create_balanced_data(input_file, output_file):
+    
+    # Balance the scenarios post-hoc so that 'yes' and 'no' are balanced
+    
+    with open(input_file, 'r') as f:
+        data = f.readlines()
+        
+    data = [x.strip() for x in data]
+    data = [x.lower() for x in data if len(x) != 0]
+    
+    # Remove the causal graph and negative edges from file
+    data = data[2:]
+    
+    # Find indices which only contain 'yes' or 'no' or contain both
+    yes_idx = []
+    no_idx = []
+    mixed_idx = []
+    mixed_diff = []
+    total_yes = 0
+    total_no = 0
+    for j in range(len(data)):
+        yes_count = data[j].count('yes.')
+        no_count = data[j].count('no.')
+        total_yes += yes_count
+        total_no += no_count
+        if yes_count > 0 and no_count == 0:
+            yes_idx.append(j)
+        elif yes_count == 0 and no_count > 0:
+            no_idx.append(j)
+        elif yes_count > 0 and no_count > 0:
+            mixed_idx.append(j)
+            mixed_diff.append(yes_count - no_count)
             
+    assert total_yes > total_no    
+    
+    # Remove some example which only have 'yes'
+    random.shuffle(yes_idx)
+    remove_idx = []
+    for idx in yes_idx:
+        remove_idx.append(idx)
+        total_yes -= data[idx].count('yes.')
+        if total_yes <= total_no:
+            break
+            
+    # If that's still not enough, remove some 'mixed' examples
+    if total_yes > total_no:
+        # Sort the idx in descending order of difference of 'yes' and 'no' so as to remove the min. no of examples 
+        mixed_idx = [idx for _, idx in sorted(zip(mixed_diff, mixed_idx), reverse=True)]
+        for idx in mixed_idx:
+            remove_idx.append(idx)
+            total_yes -= data[idx].count('yes.')
+            total_no -= data[idx].count('no.')
+            if total_yes <= total_no:
+                break
+                
+    data_balanced = [item for idx, item in enumerate(data) if idx not in remove_idx]
+    data_balanced = pd.DataFrame(data_balanced, columns=['text'])
+    data_balanced.to_csv(output_file, index=False)
 
+
+def create_type_generalization_test(input_file, output_file, source=True, target=False, framing="default", options=False):
+    
+    with open(input_file, 'r') as f:
+        data = f.readlines()
+        
+    data = [x.strip() for x in data]
+    data = [x.lower() for x in data if len(x) != 0]
+    
+    graph_info = data[0].split('.')
+    graph_edges = [x.strip().split(' causes ') for x in graph_info if len(x) != 0 and 'causes' in x and 'eventtype' not in x]
+    type_edges = [x.strip().split(' causes ') for x in graph_info if len(x) != 0 and 'causes' in x and 'eventtype' in x]
+    
+    # store in a nicer format
+    out_nodes = {}
+    for edge in graph_edges:
+        if edge[0] in out_nodes:
+            out_nodes[edge[0]].append(edge[1])
+        else:
+            out_nodes[edge[0]] = [edge[1]]
+            
+    descendant_set = get_descendants(out_nodes)
+    
+    # create a type to event dict
+    event_to_type = {}
+    type_to_event = {}
+    for x in graph_info:
+        if 'type of' in x:
+            x = x.strip().split(' ')
+            if x[-1] in type_to_event:
+                type_to_event[x[-1]].append(x[0])
+            else:
+                type_to_event[x[-1]] = [x[0]]
+                
+    # remove the events which are used in type edges (we want non-causal types only in this test)
+    for edge in type_edges:
+        _ = type_to_event.pop(edge[0], None)
+        _ = type_to_event.pop(edge[1], None)
+    
+    # create event to type afterwards so as to not include causal types
+    for node_type in type_to_event.keys():
+        for node in type_to_event[node_type]:
+            if node in event_to_type:
+                event_to_type[node].append(node_type)
+            else:
+                event_to_type[node] = [node_type]
+        
+    # create neg edges -- i.e. edges between nodes of same non-causal type as an an actual edge
+    # make sure the new edge is not possible (e.g. actual edge or transitivity)
+    neg_edges = []
+    ref_edges = []
+    for edge in graph_edges:
+        if source:
+            for edge_type in event_to_type[edge[0]]:
+                for neighbour_node in type_to_event[edge_type]:
+                    if neighbour_node != edge[0] and (neighbour_node not in descendant_set or edge[1] not in descendant_set[neighbour_node]):
+                        neg_edges.append([neighbour_node, edge[1]])
+                        ref_edges.append([edge[0], edge[1]])
+                        
+        if target:
+            for edge_type in event_to_type[edge[1]]:
+                for neighbour_node in type_to_event[edge_type]:
+                    if neighbour_node != edge[1] and neighbour_node not in descendant_set[edge[0]]:
+                        neg_edges.append([edge[0], neighbour_node])
+                        ref_edges.append([edge[0], edge[1]])
+                        
+    
+    if options:
+        test_data = {"option1": [], "option2": [], "answer": []}
+        if framing == "default":
+            option1_template = "{cause} can cause {effect}"
+            option2_template = "{cause} cannot cause {effect}"
+        elif framing == "reference":
+            option1_template = "{cause} can cause {effect}"
+            option2_template = "{cause} can cause {effect}"
+
+        for j in range(len(neg_edges)):
+            if framing == "default":
+                option1 = option1_template.format(cause=neg_edges[j][0], effect=neg_edges[j][1])
+                option2 = option2_template.format(cause=neg_edges[j][0], effect=neg_edges[j][1])
+                answer = "option2"
+            elif framing == "reference":
+                option1 = option1_template.format(cause=ref_edges[j][0], effect=ref_edges[j][1])
+                option2 = option2_template.format(cause=neg_edges[j][0], effect=neg_edges[j][1])
+                answer = "option1"
+                
+            test_data["option1"].append(option1)
+            test_data["option2"].append(option2)
+            test_data["answer"].append(answer)
+        
+        test_data = pd.DataFrame(test_data)
+        test_data.to_csv(output_file, index=False)  
+    else:
+        test_data = []
+        if framing == "default":
+            qa_template = "Answer yes or no. Can {cause} cause {effect}?"
+        elif framing == "inverted":
+            qa_template = "Answer yes or no. {cause} cannot cause {effect} - is this correct?"
+        elif framing == "counterfactual":
+            qa_template = "Answer yes or no. {cause} happened. {effect} happened. If {cause} did not happen, and {effect} has no other causes, would {effect} happen?"
+
+        for j in range(len(neg_edges)):
+            test_q = qa_template.format(cause=neg_edges[j][0], effect=neg_edges[j][1])
+            test_data.append(test_q)
+
+        test_data = pd.DataFrame(test_data, columns=['text'])
+        test_data.to_csv(output_file, index=False)
+    
+    
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='convert synthetic to readable format for finetuning')
@@ -169,21 +411,40 @@ if __name__ == "__main__":
     parser.add_argument("--create_causal_graph_test", action="store_true")
     parser.add_argument("--create_transitivity_test", action="store_true")
     parser.add_argument("--create_neg_edges_test", action="store_true")
+    parser.add_argument("--create_balanced_data", action="store_true")
+    parser.add_argument("--create_causal_graph_test_options", action="store_true")
+    parser.add_argument("--create_transitivity_test_options", action="store_true")
+    parser.add_argument("--create_type_generalization_test", action="store_true")
+    parser.add_argument("--framing", type=str, default="default")
+    parser.add_argument("--options", action="store_true")
+    parser.add_argument("--keep_causal_graph", action="store_true")
+    parser.add_argument("--ontology", action="store_true", help="create graph/edges over types instead of events")
+    parser.add_argument("--source", action="store_true", help="source type generalization")
+    parser.add_argument("--target", action="store_true", help="target type generalization")
     
-    args = parser.parse_args() 
+    
+    args = parser.parse_args()
+    
+    #seed
+    random.seed(0)
     
     if args.convert_to_csv:
-        convert_to_csv(args.data_path, args.save_path)
+        convert_to_csv(args.data_path, args.save_path, args.keep_causal_graph)
     
     if args.create_causal_graph_test:
-        create_causal_graph_test(args.data_path, args.save_path)
+        create_causal_graph_test(args.data_path, args.save_path, args.framing, args.options, args.ontology)
     
     if args.create_transitivity_test:
-        create_transitivity_test(args.data_path, args.save_path)
+        create_transitivity_test(args.data_path, args.save_path, args.framing, args.options, args.ontology)
         
     if args.create_neg_edges_test:
-        create_neg_edges_test(args.data_path, args.save_path)
-    
+        create_neg_edges_test(args.data_path, args.save_path, args.framing)
+               
+    if args.create_balanced_data:
+        create_balanced_data(args.data_path, args.save_path)
+        
+    if args.create_type_generalization_test:
+        create_type_generalization_test(args.data_path, args.save_path, args.source, args.target, args.framing, args.options)
     
     
     
